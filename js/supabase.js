701e2a1baa393f1f26d4b62db3a0859c7d20840b
Mociap <mozer.cielle@gmail.com>
2026-02-06 16:15:21 -0300

    chore: update supabase credentials and add gitignore

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
-const SUPABASE_URL = 'https://qostwvcacxpggahhrzsk.supabase.co'; // Sua URL do Supabase
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvc3R3dmNhY3hwZ2dhaGhyenNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwNDA3MTcsImV4cCI6MjA3MDYxNjcxN30.OTty2Z1BRDwJZ39H-HTEnC3aE6CZZTBKEaT59d7NAUQ'; // Sua chave pública (não exposta para leitura no cliente)
+const SUPABASE_URL = 'https://awhqkvpjgjsiqbewesqp.supabase.co'; // Nova URL do Supabase
+const SUPABASE_KEY = 'sb_publishable_X6z_d52ufsQ7gZDWAXikvw_yi_mKZBJ'; // Nova chave pública
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
